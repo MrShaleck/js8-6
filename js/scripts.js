4f21202f1458c5829f3123912a9398bf4f54c7d7
Marcin <marcinrszalek@gmail.com>
2017-05-03 15:44:21 +0200

    first-commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var numerPrzycisku = 1;
+
+switch(numerPrzycisku) {
+  case 1:
+    console.log(‘Zmieñ kolor strony.’)
+  case 2:
+    console.log(‘Zagraj melodyjkê.’)
+  case 3:
+    console.log(‘Wyœwietl wiadomoœæ.’)
+
+
+  default:
+    console.log('Ten przypadek siê wykona³, bo nie ma break\'a')
+}

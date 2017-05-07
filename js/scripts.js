3a39f5426d8fcf1f3b2adf8ba7783e1f46225c18
Marcin <marcinrszalek@gmail.com>
2017-05-07 14:34:47 +0200

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 var a = 2,
 	b = 3,
 	value = (a*a)+(2*a*b)-(b*b);
-console.log('Wynik dzia³ania wynosi: ' + value);
+console.log('Wynik dziaÅ‚ania wynosi: ' + value);
 
-if (value >= 1) {
-	console.log('Wynik dodatni');
-} else if (value <= -1) {
-	console.log('Wynik ujemny');
+if(value == 0){
+    // zero
 }
-
-if (value == 0) {
-	console.log('Wynik równy 0!');
-} else if (value != 0) {
-	console.log('Wynik ró¿ny od 0!');
+else if(value > 0){
+    // dodatnie
+}
+else {
+    // ujemne
 }
